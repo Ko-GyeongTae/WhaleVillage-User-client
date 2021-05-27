d92803e711abe13968a6505443723859bcaf429e
Ko-GyeongTae <kokt0203@naver.com>
2021-05-27 19:52:16 +0900

    <ADD> navigations

---
+++ src/navigations/MainNavigation.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from '../screens/Home';
+import MediaList from '../screens/MediaList';
+import NoticeList from '../screens/NoticeList';
+import NoticeDetail from '../screens/NoticeDetail';
+import Podbbang from '../screens/Podbbang';
+
+const Stack = createStackNavigator();
+
+export default () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home" headerMode="none">
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="MediaList" component={MediaList} />
+      <Stack.Screen name="NoticeList" component={NoticeList} />
+      <Stack.Screen name="NoticeDetail" component={NoticeDetail} />
+      <Stack.Screen name="Podbbang" component={Podbbang} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
