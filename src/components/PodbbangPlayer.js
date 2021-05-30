baf3a9ecdcedb4db0b6b882b902f0f16461d0a58
Ko-GyeongTae <kokt0203@naver.com>
2021-05-30 23:00:02 +0900

    <ADD> PodbbangPlayer.js

---
+++ src/components/PodbbangPlayer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native'
+import { WebView } from 'react-native-webview';
+
+export default ({ link }) => {
+    console.log("INSIDE PLAYER : " + link);
+    return (
+        <View style={Component.Video}>
+          <WebView
+            javaScriptEnabled={true}
+            useWebKit={true}
+            domStorageEnabled={true}
+            allowsInlineMediaPlayback={true}
+            source={{ uri: link }}
+          />
+        </View>
+    );
+}
+
+const Component = StyleSheet.create({
+    Video: { 
+      paddingTop: 10,
+      width: 350, 
+      height: 700,
+    }
+  });
