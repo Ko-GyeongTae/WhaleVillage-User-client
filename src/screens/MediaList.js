ba56ca5953f8d52b1ea760f0a85c46ebe2564c0a
Ko-GyeongTae <kokt0203@naver.com>
2021-06-02 08:17:09 +0900

    <EDIT>

--- src/screens/MediaList.js
+++ src/screens/MediaList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, ScrollView, Text, View } from "react-native";
+import { StyleSheet, ScrollView, Text, View, FlatList } from "react-native";
 import YoutubePlayer from "../components/YoutubePlayer";
 import PTRView from "react-native-pull-to-refresh";
 import { useState } from "react";
@@ -9,15 +9,16 @@ import { useEffect } from "react";
 
 export default () => {
     const [youtubeList, setYoutubeList] = useState([]);
-    const getList = async() => {
+    const getList = async () => {
         await axios.get(`${baseUri.outter_net}/api/v1/link/youtube`)
-        .then(res => {
-            setYoutubeList(res.data);
-        })
-        .catch(e => {
-            console.log(e);
-        })
-    } 
+            .then(res => {
+                setYoutubeList(res.data);
+                console.log(res.data);
+            })
+            .catch(e => {
+                console.log(e);
+            })
+    }
     const count = youtubeList.length;
     useEffect(() => {
         getList();
@@ -25,29 +26,20 @@ export default () => {
     return (
         <View style={Component.Container}>
             <View style={Style.Body}>
-                <PTRView
-                    style={Component.List}
-                    onRefresh={() => {
-                        console.log('refresh!');
-                        getList();
-                    }}
-                    pullHeight={400}
-                >
 
-                    <ScrollView
-                        contentContainerStyle={{
-                            width: "100%",
-                            height: "100%",
-                            alignItems: "center",
-                        }}
-                    >
-                        {count === 0 && <Text>게시물이 없습니다.</Text>}
-                        {youtubeList?.map(media => (
-                            console.log(media),
-                            <YoutubePlayer key={media.uid} link={media.link} />
-                        ))}
-                    </ScrollView>
-                </PTRView>
+                {count === 0 && <Text>게시물이 없습니다.</Text>}
+                <FlatList
+                    style={{
+                        width: 350,
+                        height: "100%",
+                        backgroundColor: "#f0f0f0",
+                    }}
+                    data={youtubeList}
+                    initialNumToRender={3}
+                    renderItem={({item}) => <YoutubePlayer link={item.link} />}
+                    removeClippedSubviews={true}
+                    disableVirtualization={false}
+                />
             </View>
         </View>
     );
@@ -57,6 +49,7 @@ const Style = StyleSheet.create({
     Body: {
         width: '100%',
         height: '90%',
+        alignItems: 'center',
     },
 })
 
@@ -65,7 +58,7 @@ const Component = StyleSheet.create({
         flex: 1,
         height: '100%',
     },
-    List:{
+    List: {
         backgroundColor: "#f0f0f0"
     }
 })
